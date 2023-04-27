3c1098a5bf4e86a6c2c3ff076fb985d544e59537
root <root@ns3194124.ip-152-228-225.eu>
2023-04-27 14:40:52 +0000

    updating ncut training

--- ncuts.py
+++ ncuts.py
@@ -1,55 +1,153 @@
+import os
 import pandas as pd
-import re
 import numpy as np
-import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics import silhouette_score, calinski_harabasz_score
+import matplotlib.pyplot as plt
 from sklearn.cluster import SpectralClustering
-from sklearn.preprocessing import normalize
-from sklearn.manifold import TSNE
+import time
+import pickle
 
 
-def preprocess_text(text):
-    text = re.sub('[^a-zA-Z]+', ' ', text)
-    text = text.lower()
-    return text
+# Include the spectral clustering functions from previous answers
+
+def load_data(filepath, n_sample=-1):
+    data = pd.read_json(filepath, lines=True)
+    # get the first 200 samples randomly, make sure is_deceptive contains both True and False
+    # use do while loop to make sure we get both True and False
+    test_run = False if n_sample == -1 else True
+    while test_run:
+        print(f"Trying to get {n_sample} samples, where is_deceptive contains 2 classes ...")
+        data = data.sample(n=n_sample, random_state=42)
+        if len(data['is_deceptive'].unique()) == 2:
+            break
+
+    return data
 
 
-# Load the dataset
-data = pd.read_csv('dataset/product_reviews/original/amazon_reviews.txt', delimiter='\t')
+def load_datasets(dataset_path, n_sample=-1):
+    dataset_names = [dir_name for dir_name in os.listdir(dataset_path) if os.path.isdir(os.path.join(dataset_path, dir_name))]
+    dataset_names.remove("results")
+    dataset_names.remove("figures")
+    dataset_names.remove("models")
+    datasets = []
+    print("Foud datasets...")
+    print(dataset_names)
+    for name in dataset_names:
+        cleaned_filepath = os.path.join(dataset_path, name, 'cleaned', f'{name}.json')
+        print(f"Loading {cleaned_filepath}")
+        if os.path.isfile(cleaned_filepath):
+            data = load_data(cleaned_filepath, n_sample)
+            datasets.append((name, data))
 
-# Preprocess the text
-data['REVIEW_TEXT'] = data['REVIEW_TEXT'].apply(preprocess_text)
+    print(f"{cleaned_filepath} loaded !")
+    return datasets
 
-# Vectorize the text using TF-IDF
-vectorizer = TfidfVectorizer(stop_words='english', max_features=5000)
-X = vectorizer.fit_transform(data['REVIEW_TEXT'])
+def plot_and_save_results(name, results, dataset_path):
+    print(f"Plotting {name} results ...")
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5))
 
-# Normalize the vectors
-X_normalized = normalize(X)
+    ax1.plot([result['n_clusters'] for result in results], [result['silhouette'] for result in results], label='Silhouette Score')
+    ax1.set_xlabel('Number of Clusters')
+    ax1.set_ylabel('Silhouette Score')
+    ax1.legend()
 
-# Perform Spectral Clustering using NCUT
-n_clusters = 2  # Change this value according to the number of clusters you want to create
-spectral_clustering = SpectralClustering(n_clusters=n_clusters, affinity='nearest_neighbors', random_state=42, n_neighbors=10)
-labels = spectral_clustering.fit_predict(X_normalized)
+    ax2.plot([result['n_clusters'] for result in results], [result['calinski_harabasz'] for result in results], label='Calinski-Harabasz Index')
+    ax2.set_xlabel('Number of Clusters')
+    ax2.set_ylabel('Calinski-Harabasz Index')
+    ax2.legend()
 
-# Assign the cluster labels to the dataset
-data['Cluster'] = labels
+    plt.suptitle(f'{name} - Clustering Evaluation Results')
+    plt.tight_layout()
 
-# Save the clustered data to a CSV file
-data.to_csv('dataset/product_reviews/clustered_data.csv', index=False)
+    fig_path = os.path.join(dataset_path, 'figures', 'ncut', name)
+    os.makedirs(fig_path, exist_ok=True)
+    plt.savefig(os.path.join(fig_path, 'results.png'))
+    plt.close()
 
-# Reduce dimensionality using t-SNE
-tsne = TSNE(n_components=2, random_state=42)
-X_embedded = tsne.fit_transform(X_normalized.toarray())
 
-# Plot the clusters
-plt.figure(figsize=(10, 10))
-cmap = plt.get_cmap('viridis', n_clusters)
-for i in range(n_clusters):
-    cluster_points = X_embedded[labels == i]
-    plt.scatter(cluster_points[:, 0], cluster_points[:, 1], label=f'Cluster {i}', cmap=cmap, edgecolors='k')
-plt.legend()
-plt.title('Clustered Data Visualization')
-plt.xlabel('t-SNE Component 1')
-plt.ylabel('t-SNE Component 2')
-plt.show()
+def create_directory_if_not_exists(directory):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+def save_results_as_csv(name, results, dataset_path):
+    df = pd.DataFrame(results)
+    csv_path = os.path.join(dataset_path, 'results', 'ncut', f'{name}.csv')
+    create_directory_if_not_exists(os.path.dirname(csv_path))
+    
+    # Calculate average, standard deviation, and range for Silhouette score and Calinski-Harabasz index
+    df['silhouette_mean'] = df['silhouette'].mean()
+    df['silhouette_std'] = df['silhouette'].std()
+    df['silhouette_range'] = df['silhouette'].max() - df['silhouette'].min()
+    
+    df['calinski_harabasz_mean'] = df['calinski_harabasz'].mean()
+    df['calinski_harabasz_std'] = df['calinski_harabasz'].std()
+    df['calinski_harabasz_range'] = df['calinski_harabasz'].max() - df['calinski_harabasz'].min()
+
+    df.to_csv(csv_path, index=False)
+
+
+def main(args):
+    dataset_path = args.dataset_path
+    n_sample = args.n_sample
+    datasets = load_datasets(dataset_path, n_sample)
+
+    create_directory_if_not_exists(os.path.join(dataset_path, 'figures', 'ncut'))
+    create_directory_if_not_exists(os.path.join(dataset_path, 'models', 'ncut'))
+
+    for name, data in datasets:
+        fig_path = os.path.join(dataset_path, 'figures', 'ncut', name)
+        create_directory_if_not_exists(fig_path)
+
+        model_path = os.path.join(dataset_path, 'models', 'ncut', name)
+        create_directory_if_not_exists(model_path)
+
+        X = data['text']
+        X_train, X_val = train_test_split(X, test_size=0.2, random_state=42)
+
+        vectorizer = TfidfVectorizer()
+        X_train = vectorizer.fit_transform(X_train)
+        X_val = vectorizer.transform(X_val)
+
+        MAX_CLUSTERS = 3
+        n_clusters_range = list(range(2, MAX_CLUSTERS))
+        results = []
+
+        for n_clusters in n_clusters_range:
+            start_time = time.time()
+            spectral = SpectralClustering(n_clusters=n_clusters, affinity='nearest_neighbors', n_neighbors=10, n_jobs=-1)
+            cluster_labels_train = spectral.fit_predict(X_train)
+            elapsed_time = time.time() - start_time
+
+            cluster_labels_val = spectral.fit_predict(X_val)
+
+            silhouette = silhouette_score(X_val, cluster_labels_val)
+            calinski_harabasz = calinski_harabasz_score(X_val.toarray(), cluster_labels_val)
+
+            results.append({'n_clusters': n_clusters, 'silhouette': silhouette, 'calinski_harabasz': calinski_harabasz, 'time': elapsed_time})
+
+            print(f"Dataset: {name}, n_clusters: {n_clusters}, Time elapsed: {elapsed_time} seconds")
+
+        save_results_as_csv(name, results, dataset_path)
+        plot_and_save_results(name, results, dataset_path)
+
+        best_result = max(results, key=lambda x: x['silhouette'])
+        best_n_clusters = best_result['n_clusters']
+
+        best_model = SpectralClustering(n_clusters=best_n_clusters, affinity='nearest_neighbors', n_neighbors=10, n_jobs=-1)
+        best_model.fit(X_train)
+
+        model_file = os.path.join(model_path, 'best_model.pkl')
+        with open(model_file, 'wb') as f:
+            pickle.dump(best_model, f)
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dataset_path', type=str, default='dataset', help='Path to the directory containing datasets')
+    parser.add_argument('--n_sample', '-n', type=int, default=-1, help='If > 0, only a small subset (of n_sample) of the data will be used')
+    args = parser.parse_args()
+
+    main(args)
